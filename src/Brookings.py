4db8c89bfb4cbfcdfcbcc898879b05dc99a35578
universuen <universuen@gmail.com>
2021-03-23 02:20:43 +0800

    add done

--- src/Brookings.py
+++ src/Brookings.py
@@ -36,89 +36,59 @@ class BrookingsURLManager(BaseURLManager):
 # }
 
 
-def blog_handler(url) -> Article:
-
-    ezp = EzParser(get_html(url))
-    title = ezp.find('title').text
-    data = json.loads(ezp.find('script', {'type': 'application/ld+json'}).text)
-    pic_url = data['image']['url']
-    date = data['datePublished'][:10]
-    authors = str([i['name'] for i in data['author']])
-    abstract = ezp.find('div', {'class': 'editors-note'}).find('p').text
-    content_tag = ezp.find(args={'class': 'post-body post-body-enhanced'})
-    content = ''
-    for i in content_tag.children:
-        if i.tag != 'p':
-            continue
-        content += i.text
-    category = ezp.find(args={'class': 'content-column-no-aside'}).find('ul').text.replace('\n', '').replace('\t', '')
-    return Article(
-        publisher='Brookings',
-        url=url,
-        title=title,
-        date=date,
-        authors=authors,
-        content=content,  # 这一项交给CNNSpider填
-        abstract=abstract,
-        location=None,
-        section=None,
-        category=category,
-        pic_url=pic_url,
-        type='event'
-    )
-
-
-def article_handler(url) -> Article:
-    pass
-
-
-def techstream_handler(url) -> Article:
-    pass
-
-
 class BrookinsSpider(BaseSpider):
-    def parse(self, url) -> Article:
+    def parse(self, url):
 
         if 'brookings' not in url:
             # 外链
-            pass
+            return
         elif 'blog' in url:
-            return blog_handler(url)
+            type_ = 'blog'
         elif 'article' in url:
-            return article_handler(url)
+            type_ = 'article'
         elif 'techstream' in url:
-            return techstream_handler(url)
+            type_ = 'techstream'
         else:
-            raise Exception(f'new type found:{url}')
-        # ezp = EzParser(get_html(url))
-        # title = ezp.find('title').text
-        # abstract = ezp.find('meta', {'name': 'description'})['content']
-        # content = ezp.find(
-        #     'div', {'class': 'event-description post-body'}).text
-        # authors_tags = ezp.find_all('div', {'class': 'expert-info'})
-        # authors = []
-        # for i in authors_tags:
-        #     authors.append(i.find(args={'class': 'name'}).text)
-        # authors = str(authors)
-        # date_year = ezp.find('div', {'class': 'date-year'}).text
-        # date_month = ezp.find('div', {'class': 'date-month'}).text
-        # date_month = month_map[date_month]
-        # date_number = ezp.find('div', {'class': 'date-number'}).text
-        # date = '-'.join([date_year, date_month, date_number])
-        # return Article(
-        #     publisher='Brookings',
-        #     url=url,
-        #     title=title,
-        #     date=date,
-        #     authors=authors,
-        #     content=content,  # 这一项交给CNNSpider填
-        #     abstract=abstract,
-        #     location=None,
-        #     section=None,
-        #     category=None,
-        #     pic_url=None,
-        #     type='event'
-        # )
+            return
+        ezp = EzParser(get_html(url))
+        title = ezp.find('title').text
+        data = json.loads(ezp.find('script', {'type': 'application/ld+json'}).text)
+        pic_url = data['image']['url']
+        date = data['datePublished'][:10]
+        authors = str([i['name'] for i in data['author']])
+        try:
+            abstract = ezp.find('div', {'class': 'editors-note'}).find('p').text
+        except AttributeError:
+            abstract = None
+        content_tag = ezp.find(args={'class': 'post-body post-body-enhanced'})
+        content = ''
+        for i in content_tag.children:
+            if i.tag != 'p':
+                continue
+            content += i.text
+        try:
+            category = ezp.find(
+                args={'class': 'content-column-no-aside'}
+            ).find(
+                'ul'
+            ).text.replace('\n', '').replace('\t', '')
+        except AttributeError:
+            category = None
+
+        return Article(
+            publisher='Brookings',
+            url=url,
+            title=title,
+            date=date,
+            authors=authors,
+            content=content,
+            abstract=abstract,
+            location=None,
+            section=None,
+            category=category,
+            pic_url=pic_url,
+            type=type_
+        )
 
 
 if __name__ == '__main__':
